e64cb9e29d1c9d24c238186b20e81dea88930f7a
navianwadda <navianwadda@outlook.com>
2026-09-17 07:25:19 +0600

    Add plugin management for Android and Kotlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,9 @@
 pluginManagement {
+    plugins {
+        id("com.android.application") version "8.4.0" apply false
+        id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+        id("com.chaquo.python") version "15.0.0" apply false
+    }
     repositories {
         google()
         mavenCentral()
